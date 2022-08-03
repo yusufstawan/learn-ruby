0f7e20eb0079b0d36249f58babdc8f0d4a0d7103
yusufstawan <yusufsetiyawan45@gmail.com>
2022-08-03 22:53:58 +0700

    condition & boolean

---
+++ boolean.rb
@@ -0,0 +1,11 @@
+# logic || boolean
+
+hasil1 = "saya" == "anda"
+hasil2 = "saya" != "anda"
+hasil3 = 3 >= 2
+hasil4 = 1==1 && 2==2
+
+puts hasil1
+puts hasil2
+puts hasil3
+puts hasil4
---
+++ condition.rb
@@ -0,0 +1,10 @@
+#if statement, else, elsif
+angka = 100
+
+if angka == 10
+  puts "angkanya 10"
+elsif angka == 100
+  puts "angkanya 100"
+else
+  puts "angkanya bukan 10 dan 100!"
+end
