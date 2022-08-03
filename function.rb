ed003af7ce29bf4edc099b93c3e16d7ec4fd78cf
yusufstawan <yusufsetiyawan45@gmail.com>
2022-08-03 22:38:05 +0700

    function & return

--- function.rb
+++ function.rb
@@ -1,1 +1,21 @@
 # fungsi dan parameter
+
+def jalan()
+  x = 5
+  puts "tuk.. tuk.. tuk.."
+  puts "tak.. tak.. tak.. #{x}"
+end
+
+jalan()
+
+def parameter(num)
+  puts "angkanya #{num}"
+end
+
+parameter(100)
+
+def jumlahkan(num1, num2)
+  puts "hasilnya #{num1 + num2}"
+end
+
+jumlahkan(10, 20)
---
+++ return.rb
@@ -0,0 +1,7 @@
+def jumlah(num1, num2)
+  num = num1 + num2
+  return num
+end
+
+hasil = jumlah(10, 20)
+puts hasil - 10
